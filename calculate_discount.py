885b4910cfe5af5ab1f7a8fcf58e9e0f93823b54
josephkamau32 <iamjosephkamau@gmail.com>
2025-08-12 18:42:13 +0300

    my final submission

--- calculate_discount.py
+++ calculate_discount.py
@@ -1,6 +1,16 @@
 # Question 1 answer
 def calculate_discount(price, discount_percent):
+    """
+    Calculates the final price after applying a discount if the discount is 20% or higher.
     
+    Parameters:
+    price (float): Original price of the item
+    discount_percent (float): Discount percentage to apply
+    
+    Returns:
+    float: Final price after discount (or original price if discount < 20%)
+    """
+
     if discount_percent >= 20:
         discount_amount = price * (discount_percent / 100)
         final_price = price - discount_amount
